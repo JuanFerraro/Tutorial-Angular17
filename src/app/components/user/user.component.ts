895be896c912654e750bb0a4bf47f3e7b5526682
Juan Barrios Ferraro <juansebastianferraro@gmail.com>
2024-01-23 10:36:24 -0500

    Using property binding and event handling in User Component -> src/app/components

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -11,6 +11,19 @@ export class UserComponent {
 
   username: string = 'J--n B-rr--s';
   userExist: boolean = true;
-  programmingLanguages = ['Python', 'Java', 'TypeScript', 'JavaScript']
+  programmingLanguages = ['Python', 'Java', 'TypeScript', 'JavaScript'];
+  isEditable: boolean = false; /* Used for Property Binding: Component -> Template */
+  pText: string = '🙉🙉🙉🙉🙉🙉'
+
+  editable() {
+    if (this.isEditable == true){
+      this.isEditable = false
+      this.pText = '🙉🙉🙉🙉🙉🙉'
+    } else {
+      this.isEditable = true
+      this.pText = '🙊🙊🙊🙊🙊🙊'
+    }
+    console.log(this.isEditable)
+  }
 
 }
